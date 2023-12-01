63025cf9dd0ccea86c8433335d00bb49314d7a8b
DanielArcentales <alencastrobi@gmail.com>
2023-12-01 14:10:14 -0500

    Se añade boton para mejor navegacion

--- app/finalPage/page.tsx
+++ app/finalPage/page.tsx
@@ -1,6 +1,9 @@
 import { title, subtitle } from "@/components/primitives";
 import React from "react";
 import controlUser from "../libs/controllerUser";
+import { Snippet } from "@nextui-org/snippet";
+import { Button } from "@nextui-org/react";
+import NextLink from "next/link";
 
 export default function FinalPage({ searchParams }:{
 	searchParams:{
@@ -23,6 +26,18 @@ export default function FinalPage({ searchParams }:{
 			<h2 className={subtitle({ class: "mt-4" })}>
 				Proximamente te contactaremos
 			</h2>
+			<div className="mt-8">
+				<Snippet hideSymbol hideCopyButton variant="flat">
+					<NextLink
+						color="foreground"
+						href="/"
+					>
+						<Button color="primary" variant="shadow">
+							Regresar al menu Principal
+						</Button>
+					</NextLink>
+				</Snippet>
+			</div>
 		</div>
 	);
 }
